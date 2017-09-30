9b06bc3b4434ad1d66152018a23e8eee02a7c02c
Евгений Чопенко <chopabeckham@gmail.com>
2017-09-30 16:12:38 +0300

    first

---
+++ task_01_01.py
@@ -0,0 +1,10 @@
+sotki = int(input())*100# входные параметры перевод из соткок в м^2 1ар = 100m^2
+k = 15*25# 15*25 m^2
+print (sotki,k)
+if sotki == 0:
+    print(0)
+elif sotki < k:
+    print (0)
+elif sotki >= k:
+    count = sotki-(sotki // k)*k
+    print(count)# ответ в m^2
