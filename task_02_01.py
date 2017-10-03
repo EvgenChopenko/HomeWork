2ab90e0adad89dadb8638ef2ea2e1e9bd70d3e75
Евгений Чопенко <chopabeckham@gmail.com>
2017-10-03 13:45:43 +0300

    final

--- task_02_01.py
+++ task_02_01.py
@@ -1,11 +1,14 @@
 def is_palindrome(s):
     output = list()
     s=str(s)# преобразуем все входные параметры в строки
+    s = s.strip(".")# удаляем точку в конце и в начале
     for w in s.split():
-        output.append(w)# удаляем пробелы в тексте
+        output.append(w.strip(','))# удаляем пробелы в тексте и запятые
     s =(''.join([str(m) for m in output]))# преобразуем list в строку строка без пробелов
     s=s.lower()#все символы без регистра
     if s == s[::-1]:# если строка равна с реверсом то TRUE ибо False
         return True
     else:
         return False
+
+
