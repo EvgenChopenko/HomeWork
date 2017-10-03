31359263f02088b75ca093fde3e420a1e42fde3e
Евгений Чопенко <chopabeckham@gmail.com>
2017-10-03 12:45:34 +0300

    BAg task_01_03

--- task_01_03.py
+++ task_01_03.py
@@ -1,31 +1,14 @@
 lst = list()
 while lst.__len__() < 3:
     lst.append(int(input()))
-
-# метод пузырька
-n=lst.__len__()-1
-
-i=0# первый элемент
-j=1# i+1
-a=None# промежуточный коэфициент для сортировки
-while i <n:
-    while j<n+1:
-        if lst[i]>lst[j]:
-            a=lst[j]
-            lst[j]=lst[i]
-            lst[i]=a
-        j=j+1
-        i=i+1
+n=1
 i=0
-j=1
-while i <n:
-    while j<n+1:
-        if lst[n-i]<lst[n-j]:
-            a=lst[n-j]
-            lst[n-j]=lst[n-i]
-            lst[n-i]=a
-        j=j+1
-        i=i+1
+while n < len(lst):
+    for i in range(len(lst) - n):
+        if lst[i] > lst[i + 1]:
+            lst[i], lst[i + 1] = lst[i + 1], lst[i]
+            print(i)
+    n += 1
 
 
     print(','.join([str(m) for m in lst]))# ужасный способ вывода ((
---
+++ task_02_01.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    output = list()
+    s=str(s)# преобразуем все входные параметры в строки
+    for w in s.split():
+        output.append(w)# удаляем пробелы в тексте
+    s =(''.join([str(m) for m in output]))# преобразуем list в строку строка без пробелов
+    s=s.lower()#все символы без регистра
+    if s == s[::-1]:# если строка равна с реверсом то TRUE ибо False
+        print(True)
+    else:
+        print(False)
