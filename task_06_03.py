597d996b32acca8a801739cd9fc4b6a6feff9575
Евгений Чопенко <chopabeckham@gmail.com>
2017-10-18 05:25:21 +0300

    Nturple

---
+++ task_06_03.py
@@ -0,0 +1,48 @@
+
+from collections import namedtuple
+
+
+def return_namedtuple(*args):
+
+    def decarator(func):
+        Nturple = namedtuple('Nturple',['name','digital'])
+        def wrapper(*argsdunc,**kwargsfunc):
+            s=func(*argsdunc,**kwargsfunc)
+            if(isinstance(s,tuple)):
+                p=Nturple (name=args, digital=s)
+                return (p)
+            else:
+                 return s
+        return wrapper
+    return decarator
+
+
+
+
+
+
+
+
+
+
+if(__name__=='__main__'):
+    @return_namedtuple('one', 'two')
+    def func1():
+        return 1, 2
+
+
+    @return_namedtuple('one', 'two', 'three')
+    def func2():
+        return 1, 2, 3
+
+    print(func1())
+    print(func2())
+
+
+    @return_namedtuple('one')
+    def func3():
+        return 1
+
+
+    print(func3())
+
