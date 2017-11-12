e14304fb5b67eb4f6247fb13444218617a188e20
Евгений Чопенко <chopabeckham@gmail.com>
2017-11-12 23:02:04 +0300

    generator

--- task_07_02.py
+++ task_07_02.py
@@ -6,7 +6,8 @@ def password_generator(n):
     str2= "qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+-=?><{}:|\/"
     str3=str2.upper()
     str3=str3+str2+str(str1)
-    return "".join([random.choice(str3) for i in range(n)])
+    return (random.choice(str3) for i in range(n))
+
 
 if (__name__=="__main__"):
     print (password_generator(16))
